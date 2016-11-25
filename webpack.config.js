771e40815469d11e59a7d401c9d499f773533b2f
won <wjin0352@gmail.com>
2016-11-25 02:22:27 -0500

    got rid of babel preset in config

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,7 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-object-rest-spread']
-        }
+      loader: 'babel'
     }]
   },
   resolve: {
