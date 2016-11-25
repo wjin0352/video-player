56db247ad92289fbd1c7a89727e30e7bffc3a0df
won <wjin0352@gmail.com>
2016-11-25 18:23:32 -0500

    routing and pushing to prod

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
-
+require('babel-register');
+require('babel-polyfill');
 let app = express();
 
 const variables = require('./server/config/variables.express');
@@ -8,7 +9,7 @@ require('./server/config/mongoose.connection');
 require('./server/config/config.express')(app);
 require('./server/config/routes.express')(app);
 
-// to push to production heroku you need to listen on process.env.PORT
+// to push to production heroku you need to listen on port given from heroku on process.env.PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- server/config/routes.express.js
+++ server/config/routes.express.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const videoRouter = require('../api/video/video.router');
 const mainRouter = require('../api/main/main.router');
 
 module.exports = (app) => {
   app.use('/', mainRouter);
   app.use('/', videoRouter);
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('./public/index.html'))
+  })
 }
